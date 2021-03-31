55e2d56162febee15ec80d7d28b37e8dcb1772b6
Chris <christopher-dags@hotmail.com>
2021-03-31 17:41:35 -0400

    Added BeerOrderLineMapperDecorator, BeerDto, BeerService, and BeerServiceImpl

--- src/main/java/guru/sfg/beer/order/service/web/mappers/BeerOrderMapper.java
+++ src/main/java/guru/sfg/beer/order/service/web/mappers/BeerOrderMapper.java
@@ -19,7 +19,9 @@ package guru.sfg.beer.order.service.web.mappers;
 
 import guru.sfg.beer.order.service.domain.BeerOrder;
 import guru.sfg.beer.order.service.web.model.BeerOrderDto;
+import org.mapstruct.DecoratedWith;
 import org.mapstruct.Mapper;
+
 
 @Mapper(uses = {DateMapper.class, BeerOrderLineMapper.class})
 public interface BeerOrderMapper {
--- target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderMapperImpl.java
+++ target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderMapperImpl.java
@@ -17,7 +17,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-03-29T10:37:36-0400",
+    date = "2021-03-29T13:09:30-0400",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 15.0.2 (Oracle Corporation)"
 )
 @Component
