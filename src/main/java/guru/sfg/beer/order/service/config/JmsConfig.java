e26a81492dd0d7e753405b8e5d6c6af7a6c8c3e2
Chris <christopher-dags@hotmail.com>
2021-04-19 15:42:28 -0400

    Completed Assignment 7: Place Validate Order Request. Closes #11

--- src/main/java/guru/sfg/beer/order/service/config/JmsConfig.java
+++ src/main/java/guru/sfg/beer/order/service/config/JmsConfig.java
@@ -8,6 +8,9 @@ import org.springframework.jms.support.converter.MessageType;
 
 @Configuration
 public class JmsConfig {
+
+    public static final String VALIDATE_ORDER_QUEUE = "validate-order";
+
     @Bean // Serialize message content to json
     public MessageConverter jacksonJmsMessageConverter(){
         MappingJackson2MessageConverter mappingJackson2MessageConverter = new MappingJackson2MessageConverter();
